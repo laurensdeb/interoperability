8ba453eacf4432bf370df6ce0a156e951607467b
Laurens Debackere <Laurens.Debackere@UGent.be>
2022-03-19 18:32:49 +0100

    fix: fixes build failure and adds configuration for uma endpoint

--- packages/aa-uma/src/index.ts
+++ packages/aa-uma/src/index.ts
@@ -9,4 +9,17 @@ export * from './secrets/JwksKeyHolder';
 export * from './secrets/InMemoryJwksKeyHolder';
 
 // Token
-export * from './token/MockUmaGrantProcessor';
+export * from './token/UmaGrantProcessor';
+export * from './token/GrantTypeProcessor';
+export * from './token/TokenFactory';
+export * from './token/TicketFactory';
+export * from './token/JwtTokenFactory';
+export * from './token/JwtTicketFactory';
+
+// Token/Authn
+export * from './token/authn/ClaimTokenProcessor';
+export * from './token/authn/DpopClaimTokenProcessor';
+
+// Token/Authz
+export * from './token/authz/Authorizer';
+export * from './token/authz/AllAuthorizer';
