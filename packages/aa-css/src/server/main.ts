a602729971fb9df88b68912441507c8a0760f967
Laurens Debackere <Laurens.Debackere@UGent.be>
2022-03-24 08:40:08 +0100

    feat(authorization-agent-css): Added CSS development environment

---
+++ packages/aa-css/src/server/main.ts
@@ -0,0 +1,28 @@
+import {absoluteFilePath, App, AppRunner} from '@solid/community-server';
+import {IComponentsManagerBuilderOptions} from 'componentsjs';
+import * as path from 'path';
+
+export const runServer = async () => {
+  const appRunner = new AppRunner();
+
+  console.info(absoluteFilePath('.'));
+  console.info(path.join(__dirname, '../../config/default.json'));
+
+  const configVariables = {
+    'urn:solid-server:default:variable:showStackTrace': true,
+    'urn:solid-server:default:variable:loggingLevel': 'Info',
+    'urn:solid-server:default:variable:port': 3000,
+    'urn:solid-server:default:variable:baseUrl': 'http://localhost:3000',
+  };
+
+  const app = await appRunner.create(
+      {
+        mainModulePath: absoluteFilePath('.'),
+      },
+      path.join(__dirname, '../../config/default.json'),
+      configVariables,
+  );
+
+  await app.start();
+};
+runServer();
