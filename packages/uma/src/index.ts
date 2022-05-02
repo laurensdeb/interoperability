220d911d0cf3400f740aa443b3e24ab6888a61ac
Laurens Debackere <Laurens.Debackere@UGent.be>
2022-05-02 19:08:33 +0200

    feat(sai-uma): minor fix

--- packages/uma/src/index.ts
+++ packages/uma/src/index.ts
@@ -3,20 +3,23 @@ export * from './http/UmaConfigRequestHandler';
 export * from './http/PermissionRegistrationHandler';
 export * from './http/TokenRequestHandler';
 
-
 // Token
-export * from './token/UmaGrantProcessor';
-export * from './grant/GrantTypeProcessor';
 export * from './token/TokenFactory';
-export * from './ticket/TicketFactory';
 export * from './token/JwtTokenFactory';
-export * from './token/JwtTicketFactory';
 
-// Token/Authn
+// Grant
+export * from './grant/UmaGrantProcessor';
+export * from './grant/GrantTypeProcessor';
+
+// Ticket
+export * from './ticket/TicketFactory';
+export * from './ticket/JwtTicketFactory';
+
+// Authn
 export * from './authn/ClaimTokenProcessor';
 export * from './authn/DpopClaimTokenProcessor';
 export * from './authn/BasicClaimTokenProcessor';
 
-// Token/Authz
+// Authz
 export * from './authz/AllAuthorizer';
 export * from './authz/util/UmaFetchFactory';
